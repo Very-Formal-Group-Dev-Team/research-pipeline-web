a398bdc93d16b21a0d9b80815f31908df615ac1e
unknown <joshuafrellt@gmail.com>
2026-06-03 03:43:21 +0800

    feat: booking a meeting directs user to meetings section of project details

---
+++ lib/meetings/navigation.ts
@@ -0,0 +1,6 @@
+/** DOM id for the Meetings block on adviser project detail. */
+export const PROJECT_MEETINGS_SECTION_ID = 'project-meetings';
+
+export function adviserProjectMeetingsUrl(projectId: string): string {
+  return `/adviser/advisees/${projectId}#${PROJECT_MEETINGS_SECTION_ID}`;
+}
